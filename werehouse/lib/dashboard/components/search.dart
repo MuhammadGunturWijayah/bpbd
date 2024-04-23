2cde38c6f78ae197c7b5365f511634c277a6a76a
guntur <gunturwijayah@gmail.com>
2024-04-23 22:37:48 +0700

    menambahkan nama pengguna

--- werehouse/lib/dashboard/components/search.dart
+++ werehouse/lib/dashboard/components/search.dart
@@ -8,33 +8,17 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 23, left: 15, right: 15),
+      padding: const EdgeInsets.only(top: 50, left: 15, right: 15),
       child: Row(
         children: [
           Flexible(
             fit: FlexFit.tight,
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  color: const Color(0xFFFAFAFA),
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: const Color(0xFFE8E8E8))),
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/search.svg',
-                    color: dark1,
-                    width: 20,
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    'Cari layanan, makanan, & tujuan',
-                    style: regular14.copyWith(color: dark3),
-                  ),
-                ],
+            child: Text(
+              'Hallo, Muhammad Guntur Wijaya',
+              style: TextStyle(
+                fontSize: 18, // Ubah ukuran font di sini
+                fontWeight: FontWeight.bold, // Tambahkan tebal
+                color: Colors.black,
               ),
             ),
           ),
@@ -42,33 +26,37 @@ class Search extends StatelessWidget {
             width: 15,
           ),
           SizedBox(
-              width: 35,
-              height: 35,
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35 / 2)),
-                    clipBehavior: Clip.hardEdge,
-                    child: Image.asset('assets/images/avatar.png'),
+            width: 35,
+            height: 35,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(35 / 2),
                   ),
-                  Positioned(
-                      right: 0,
-                      bottom: 0,
-                      child: Container(
-                        width: 16,
-                        height: 16,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(35 / 2),
-                            color: const Color(0xFFD1E7EE)),
-                        clipBehavior: Clip.hardEdge,
-                        child: SvgPicture.asset(
-                          'assets/icons/goclub.svg',
-                          color: blue3,
-                        ),
-                      ))
-                ],
-              ))
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset('assets/images/avatar.png'),
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(35 / 2),
+                      color: const Color(0xFFD1E7EE),
+                    ),
+                    clipBehavior: Clip.hardEdge,
+                    child: SvgPicture.asset(
+                      'assets/icons/goclub.svg',
+                      color: blue3,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
