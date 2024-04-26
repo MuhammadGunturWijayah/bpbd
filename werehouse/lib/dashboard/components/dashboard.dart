4d65862b8917e12fd7c4a5dcaf32d8d995b78936
guntur <gunturwijayah@gmail.com>
2024-04-26 22:30:40 +0700

    menambahkan fungsi penggeseran gambar pada dashboard

--- werehouse/lib/dashboard/components/dashboard.dart
+++ werehouse/lib/dashboard/components/dashboard.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:werehouse/dashboard/fitur_button_dashboard.dart';
 import 'package:werehouse/theme.dart';
 
-class Gopay extends StatelessWidget {
+class Gopay extends StatefulWidget {
   const Gopay({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    int currentImageIndex = 0;
-    List<String> images3 = [
-      "assets/images/1.png",
-    ];
+  _GopayState createState() => _GopayState();
+}
 
+class _GopayState extends State<Gopay> {
+  int currentImageIndex = 0;
+  List<String> images3 = [
+    "assets/images/1.png",
+    "assets/images/2.png",
+    "assets/images/3.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(
         left: 15,
@@ -21,32 +28,32 @@ class Gopay extends StatelessWidget {
       child: Container(
         height: 180,
         decoration: BoxDecoration(
-          color: blue1,
+          color: Colors.white,
           borderRadius: BorderRadius.circular(15),
         ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
+        child: PageView.builder(
+          itemCount: images3.length,
+          onPageChanged: (index) {
+            setState(() {
+              currentImageIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            return Padding(
               padding: const EdgeInsets.only(left: 10, right: 8),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(15),
                 child: Container(
                   width: 344,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(images3[
-                          currentImageIndex]), // Ganti dengan path gambar dari list 'images3' dan indeks 'currentImageIndex'
-                      fit: BoxFit.cover,
-                    ),
+                  height: 180,
+                  child: Image.asset(
+                    images3[index],
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-            ),
-
-            // Other widgets can go here if needed
-          ],
+            );
+          },
         ),
       ),
     );
