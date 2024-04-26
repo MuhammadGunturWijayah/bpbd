00cd6a9879d35e35bbde9ea14aaa49416d554d86
guntur <gunturwijayah@gmail.com>
2024-04-26 22:46:39 +0700

    menambahkan titik tiga pada iklan dashboard

--- werehouse/lib/dashboard/components/dashboard.dart
+++ werehouse/lib/dashboard/components/dashboard.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:werehouse/dashboard/fitur_button_dashboard.dart';
-import 'package:werehouse/theme.dart';
 
 class Gopay extends StatefulWidget {
   const Gopay({Key? key}) : super(key: key);
@@ -31,31 +29,70 @@ class _GopayState extends State<Gopay> {
           color: Colors.white,
           borderRadius: BorderRadius.circular(15),
         ),
-        child: PageView.builder(
-          itemCount: images3.length,
-          onPageChanged: (index) {
-            setState(() {
-              currentImageIndex = index;
-            });
-          },
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 10, right: 8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Container(
-                  width: 344,
-                  height: 180,
-                  child: Image.asset(
-                    images3[index],
-                    fit: BoxFit.cover,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView.builder(
+              itemCount: images3.length,
+              onPageChanged: (index) {
+                setState(() {
+                  currentImageIndex = index;
+                });
+                // Check if user reached the last image, then reset to the first image
+                if (index == images3.length - 1) {
+                  Future.delayed(Duration(milliseconds: 500), () {
+                    // Add a delay to give smooth transition effect
+                    _resetToFirstImage();
+                  });
+                }
+              },
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 8),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      width: 344,
+                      height: 180,
+                      child: Image.asset(
+                        images3[index],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            Positioned(
+              bottom: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  images3.length,
+                  (index) => Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    width: 8.0,
+                    height: 8.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: currentImageIndex == index
+                          ? const Color.fromARGB(255, 255, 255, 255) // Warna untuk menandakan gambar aktif
+                          : Colors.grey, // Warna untuk menandakan gambar tidak aktif
+                    ),
                   ),
                 ),
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  // Method to reset the PageView to the first image
+  void _resetToFirstImage() {
+    setState(() {
+      currentImageIndex = 0;
+    });
   }
 }
