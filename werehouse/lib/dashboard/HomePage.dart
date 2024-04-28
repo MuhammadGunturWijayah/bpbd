b0b9463094900e97fda6f410274a6d3077a13970
guntur <gunturwijayah@gmail.com>
2024-04-28 19:36:11 +0700

    menambahkan navigation pada tampilan dashboard

--- werehouse/lib/dashboard/HomePage.dart
+++ werehouse/lib/dashboard/HomePage.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:werehouse/dashboard/components/info_lainnya.dart';
-import 'package:werehouse/dashboard/components/notif_permintaan.dart';
 import 'package:werehouse/dashboard/components/dashboard.dart';
+import 'package:werehouse/dashboard/components/info_lainnya.dart';
 import 'package:werehouse/dashboard/components/menus.dart';
 import 'package:werehouse/dashboard/components/news.dart';
+import 'package:werehouse/dashboard/components/notif_permintaan.dart';
 import 'package:werehouse/dashboard/components/search.dart';
-import 'package:werehouse/dashboard/components/navigation_bar.dart';
-import 'package:werehouse/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,17 +14,41 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Search(),
-          dashboard(),
-          Menus(),
-          GoCLub(),
-          Akses(),
-          News(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Search(),
+            dashboard(),
+            Menus(),
+            GoCLub(),
+            Akses(),
+            News(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.scanner),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
         ],
-      )),
+        selectedItemColor: Colors.blue, 
+        unselectedItemColor: Colors.grey, 
+        currentIndex: 0, 
+        onTap: (index) {
+         
+        },
+      ),
     );
   }
 }
+
