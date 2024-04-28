9c65e4873224de15240e51047b87286ed261ea6b
guntur <gunturwijayah@gmail.com>
2024-04-29 00:55:25 +0700

    .

--- werehouse/lib/dashboard/HomePageIcon.dart
+++ werehouse/lib/dashboard/HomePageIcon.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class GojekIcon {
+class button_icon {
   final String icon;
   final String title;
   final Color? color;
 
 
-  GojekIcon({required this.icon, required this.title, this.color});
+  button_icon({required this.icon, required this.title, this.color});
 }
--- werehouse/lib/dashboard/fitur_button_dashboard.dart
+++ werehouse/lib/dashboard/fitur_button_dashboard.dart
@@ -5,14 +5,14 @@ import 'package:werehouse/dashboard/HomePageIcon.dart';
 import 'package:werehouse/dashboard/profile.dart';
 import 'package:werehouse/theme.dart';
 
-List<GojekIcon> menuIcons = [
+List<button_icon> menuIcons = [
 
-  GojekIcon(icon: 'laporan', title: 'Laporan', color: green2,),
-  GojekIcon(icon: 'scan', title: 'Scan', color: green2,),
-  GojekIcon(icon: 'bantuan', title: 'Bantuan', color: red, ),
-  GojekIcon(icon: 'barang', title: 'Barang', color: green2, ),
-  GojekIcon(icon: 'setting', title: 'Pengaturan', color: red, ),
-  GojekIcon(icon: 'scan', title: 'Akun', color: blue2, ),
-  GojekIcon(icon: 'goclub', title: 'Tambah Akun', color: purple, ),
-  GojekIcon(icon: 'other', title: 'Lainnya', color: dark4, ),
+  button_icon(icon: 'laporan', title: 'Laporan', color: green2,),
+  button_icon(icon: 'scan', title: 'Scan', color: green2,),
+  button_icon(icon: 'bantuan', title: 'Bantuan', color: red, ),
+  button_icon(icon: 'barang', title: 'Barang', color: green2, ),
+  button_icon(icon: 'setting', title: 'Pengaturan', color: red, ),
+  button_icon(icon: 'scan', title: 'Akun', color: blue2, ),
+  button_icon(icon: 'goclub', title: 'Tambah Akun', color: purple, ),
+  button_icon(icon: 'other', title: 'Lainnya', color: dark4, ),
 ];
