13a64919c01783ca670c3ba5e08217ae1c56d2da
guntur <gunturwijayah@gmail.com>
2024-04-30 17:51:37 +0700

    fitur laporan

---
+++ werehouse/lib/dashboard/component_laporan/doctor_model.dart
@@ -0,0 +1,42 @@
+//import 'package:WEREHOUSE/dashboard/homepage/laporan_dashboard/service_model.dart';
+
+class DoctorModel {
+  String name;
+  String image;
+  List<String> services;
+  String distance;
+
+  DoctorModel({
+    required this.name,
+    required this.image,
+    required this.services,
+    required this.distance,
+  });
+}
+
+var doctors = [
+  DoctorModel (
+    name: "Barang",
+    image: "gambar_laporan.png", // Memperbarui path gambar disini
+    services: ["Laporan Barang"],
+    distance: "",
+  ),
+  DoctorModel(
+    name: "Penerimaan Logistik",
+    image: "gambar_laporan.png", // Memperbarui path gambar disini
+    services: ["Laporan Penerimaan Logistik"],
+    distance: "",
+  ),
+  DoctorModel(
+    name: "Pengeluaran Logistik",
+    image: "gambar_laporan.png", // Memperbarui path gambar disini
+    services: ["Laporan Pengeluaran Logistik"],
+    distance: "",
+  ),
+  DoctorModel(
+    name: "Rekab Kejadian",
+    image: "gambar_laporan.png", // Memperbarui path gambar disini
+    services: ["Laporan Kejadian Bencana"],
+    distance: "",
+  ),
+];
